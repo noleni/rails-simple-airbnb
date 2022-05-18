02540007a0147a4905a41104ef4897e20176394f
Marine Ramillon <marine.ramillon@gmail.com>
2022-05-18 16:57:09 +0200

    adding items to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+studio = { name: "Studio", address: "7 Boundary St, London E2 7JE", description: 'joli', }
+loft = { name: "Loft", address: "56A Shoreditch High St, London E1 6PQ", category: 'charmant' }
+chalet = { name: "Chalet", address: "rue du Cher", category: 'en bois' }
+
+[studio, loft, chalet].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
